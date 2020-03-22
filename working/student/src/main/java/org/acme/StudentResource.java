2872b62352ff8c029622a5043b25dd3693f1634d
jclingan <jclingan@redhat.com>
2020-03-22 12:54:10 -0700

    Finished Hands-On Config

--- working/student/src/main/java/org/acme/StudentResource.java
+++ working/student/src/main/java/org/acme/StudentResource.java
@@ -2,14 +2,24 @@ package org.acme;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
 @Path("/student")
 public class StudentResource {
+    @Inject
+    @ConfigProperty(name = "delay", defaultValue = "2000")
+    int delay;
+
+    @Inject
+    @ConfigProperty(name = "students")
     List<String> students = new ArrayList<>();
 
     @GET
@@ -22,6 +32,18 @@ public class StudentResource {
     @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
     public List<String> listStudents() {
+        doDelay();
         return students;
+    }
+
+    void doDelay() {
+        int delayTime;
+        try {
+            delayTime = delay;
+            System.out.println("** Waiting " + delayTime + "ms **");
+            TimeUnit.MILLISECONDS.sleep(delayTime);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
